dbe27411de72f4978968c5201a3ba9c5f45252a0
VanshAgarwal23 <vanshagrawalitarsi23@gmail.com>
2026-09-13 21:22:25 +0530

    Implement calculator MUX using logic gates

--- 4-bit binary Calculator using Verilog.srcs/sources_1/new/mux.v
+++ 4-bit binary Calculator using Verilog.srcs/sources_1/new/mux.v
@@ -12,18 +12,19 @@ module mux(
     // NOT gate
     not (NOT_SEL, SEL);
 
-    // AND gates
+    // AND gates for addition path
     and (ADD_PATH[0], ADD_RESULT[0], NOT_SEL);
     and (ADD_PATH[1], ADD_RESULT[1], NOT_SEL);
     and (ADD_PATH[2], ADD_RESULT[2], NOT_SEL);
     and (ADD_PATH[3], ADD_RESULT[3], NOT_SEL);
 
+    // AND gates for subtraction path
     and (SUB_PATH[0], SUB_RESULT[0], SEL);
     and (SUB_PATH[1], SUB_RESULT[1], SEL);
     and (SUB_PATH[2], SUB_RESULT[2], SEL);
     and (SUB_PATH[3], SUB_RESULT[3], SEL);
 
-    // OR gates
+    // OR gates to generate final result
     or (RESULT[0], ADD_PATH[0], SUB_PATH[0]);
     or (RESULT[1], ADD_PATH[1], SUB_PATH[1]);
     or (RESULT[2], ADD_PATH[2], SUB_PATH[2]);
