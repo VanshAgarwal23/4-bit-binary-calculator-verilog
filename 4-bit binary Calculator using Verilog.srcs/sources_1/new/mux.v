7c6594d467ba976809acee3c8eb61ca6fcc1f13a
VanshAgarwal23 <vanshagrawalitarsi23@gmail.com>
2026-09-13 21:01:46 +0530

    Implement 4-bit MUX using logic gates

--- 4-bit binary Calculator using Verilog.srcs/sources_1/new/mux.v
+++ 4-bit binary Calculator using Verilog.srcs/sources_1/new/mux.v
@@ -5,6 +5,28 @@ module mux(
     output [3:0] RESULT
 );
 
-    assign RESULT = SEL ? SUB_RESULT : ADD_RESULT;
+    wire NOT_SEL;
+    wire [3:0] ADD_PATH;
+    wire [3:0] SUB_PATH;
+
+    // NOT gate
+    not (NOT_SEL, SEL);
+
+    // AND gates
+    and (ADD_PATH[0], ADD_RESULT[0], NOT_SEL);
+    and (ADD_PATH[1], ADD_RESULT[1], NOT_SEL);
+    and (ADD_PATH[2], ADD_RESULT[2], NOT_SEL);
+    and (ADD_PATH[3], ADD_RESULT[3], NOT_SEL);
+
+    and (SUB_PATH[0], SUB_RESULT[0], SEL);
+    and (SUB_PATH[1], SUB_RESULT[1], SEL);
+    and (SUB_PATH[2], SUB_RESULT[2], SEL);
+    and (SUB_PATH[3], SUB_RESULT[3], SEL);
+
+    // OR gates
+    or (RESULT[0], ADD_PATH[0], SUB_PATH[0]);
+    or (RESULT[1], ADD_PATH[1], SUB_PATH[1]);
+    or (RESULT[2], ADD_PATH[2], SUB_PATH[2]);
+    or (RESULT[3], ADD_PATH[3], SUB_PATH[3]);
 
 endmodule
